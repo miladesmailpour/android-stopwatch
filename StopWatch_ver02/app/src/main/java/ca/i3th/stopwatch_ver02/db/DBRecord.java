8a43b23085a3f45baf5597906bb6b58d37c8b1ef
milad <milad.esmailpour@gmail.com>
2021-01-03 02:19:06 -0500

    Home fragment class and layout implemented

--- StopWatch_ver02/app/src/main/java/ca/i3th/stopwatch_ver02/db/DBRecord.java
+++ StopWatch_ver02/app/src/main/java/ca/i3th/stopwatch_ver02/db/DBRecord.java
@@ -1,6 +1,9 @@
 package ca.i3th.stopwatch_ver02.db;
 
+import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
+@Database(entities = {Record.class}, version = 1)
 public abstract class DBRecord extends RoomDatabase {
+    public abstract RecordDao recordDao();
 }
