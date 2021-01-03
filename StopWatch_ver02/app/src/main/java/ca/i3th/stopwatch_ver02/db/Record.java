5327f91d90d69cb9cd87d46db6f66b78911c48c5
milad <milad.esmailpour@gmail.com>
2021-01-03 01:49:54 -0500

    Data Entites for db implemented.

--- StopWatch_ver02/app/src/main/java/ca/i3th/stopwatch_ver02/db/Record.java
+++ StopWatch_ver02/app/src/main/java/ca/i3th/stopwatch_ver02/db/Record.java
@@ -1,4 +1,55 @@
 package ca.i3th.stopwatch_ver02.db;
 
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity (tableName = "records")
 public class Record {
+
+    @PrimaryKey
+    private int id;
+    private String date;
+    private String time;
+    private String recordedTime;
+    private String details;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getRecordedTime() {
+        return recordedTime;
+    }
+
+    public void setRecordedTime(String recordedTime) {
+        this.recordedTime = recordedTime;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
 }
